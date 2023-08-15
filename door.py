6fa1f382b9b819106abd8c8cd89861c7e007e04a
Marcel Suter <marcel.suter@bzz.ch>
2023-08-15 14:55:25 +0200

    initial commit

--- door.py
+++ door.py
@@ -3,7 +3,7 @@ class Door:
     Diese Klasse beschreibt eine Türe mit der Eigenschaft color (Farbe) und den Zuständen
     door_is_open (für geöffnete Türe) sowie door_is_locked (für verriegelte Türe).
     Die Türe überwacht die beiden Zustände und verhindert so Aktionen, die nicht möglich sind.
-    Das verriegeln selber delegiert die Türe an ein Objekt vom Typ Door_lock (Türschloss).
+    Das Verriegeln selber delegiert die Türe an ein Objekt vom Typ Door_lock (Türschloss).
     """
 
     # Mit dem Keyword def wird eine Funktion bzw. eben ein Konstruktor deklariert.
